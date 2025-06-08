73e4a8ffd930707c10fe63c92c2eb2f422f5e576
shaulapg <149414814+shaulapg@users.noreply.github.com>
2025-06-08 17:46:11 -0600

    Use of lambda

--- racket.rkt
+++ racket.rkt
@@ -17,42 +17,40 @@
 
   (define best (box 0.0))
   (define bestwin (box 1))
-  (define loss 1)
 
-  (let loop-loss ()
-    (when (not (= p 0))
-      (define prev 0.0)
-      (define cont #f)
-      (define win (unbox bestwin))
+  (let loop-loss ((loss 1))
+  (when (not (= p 0))
+    (define cont (box #f))
+    (let ((prev (box 0.0))
+          (win (box (unbox bestwin))))
+      ((lambda ()
+         (let loop-win ()
+           (define cur (solve p refund (unbox win) loss))
+           (when (> cur (unbox best))
+             (set-box! best cur)
+             (set-box! bestwin (unbox win))
+             (set-box! cont #t))
+           (when (>= cur (unbox prev))
+             (set-box! prev cur)
+             (set-box! win (+ 1 (unbox win)))
+             (loop-win)))))
+      (when (unbox cont)
+        (loop-loss (+ loss 1))))))
 
-      (let loop-win ()
-        (define cur (solve p refund win loss))
-        (when (> cur (unbox best))
-          (set-box! best cur)
-          (set-box! bestwin win)
-          (set! cont #t))
-        (when (>= cur prev)
-          (set! prev cur)
-          (set! win (+ win 1))
-          (loop-win)))
-
-      (when cont
-        (set! loss (+ loss 1))
-        (loop-loss))))
-  
   (unbox best))
 
-(max-expected-profit 0 49.9)
+
+;(max-expected-profit 0 49.9)
 ;output: 0.00000000
 
-(max-expected-profit 50 49.85)
+;(max-expected-profit 50 49.85)
 ;output: 7.101784529895273
 
-(max-expected-profit 100 10.3)
+;(max-expected-profit 100 10.3)
 ;output: 0.11482720
 
-(max-expected-profit 94 12.45)
+;(max-expected-profit 94 12.45)
 ;output: 0.07197000
 
-(max-expected-profit 80 39.74)
+;(max-expected-profit 80 39.74)
 ;output: 0.33154559
